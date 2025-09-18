6a03c4a3e4650526adecb2e32ad3966965438b07
sametozlu <samedozlu@icloud.com>
2025-09-18 10:10:39 +0300

    fix(vercel): replace app/robots.txt and sitemap.xml routes with app/robots.ts and app/sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,4 @@
-import { type MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const base = process.env.NEXTAUTH_URL ?? "http://localhost:3000";
